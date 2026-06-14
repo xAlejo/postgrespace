72b16349ccc0d345b37c4504bc602d9b5432a8a4
alvaro neira <alvaro.neira.2233@gmail.com>
2026-06-14 18:51:13 -0400

    Update paso1.sql

--- entregas/alvaro_neira/0.1-veterinaria/paso1.sql
+++ entregas/alvaro_neira/0.1-veterinaria/paso1.sql
@@ -1,1 +1,8 @@
-
+INSERT INTO tutores (nombre, telefono) VALUES
+('juanito', '2334324'),
+('tamara', '2237812');
+SELECT * FROM tutores;
+UPDATE tutores
+SET telefono = '444-4444'
+WHERE nombre = 'Carlos Mendoza';
+SELECT * FROM tutores WHERE nombre = 'Carlos Mendoza';
